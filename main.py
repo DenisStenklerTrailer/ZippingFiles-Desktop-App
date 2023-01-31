d6b6605b68b762eb90211db6763b067b8ac875c0
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-01-31 20:58:09 +0100

    Working ZIP creator app
    Done with desktop ZIP compressor app.

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import PySimpleGUI as sg
+import ZIP_Creator as ZIP
 
 #line 1
 label1 = sg.Text("Select files to ZIP")
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose")
+choose_button1 = sg.FilesBrowse("Choose", key="files")
 #line 2
 label2 = sg.Text("Select to where you want to ZIP")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose")
+choose_button2 = sg.FolderBrowse("Choose",key="folder")
 
 compress_button = sg.Button("Compress")
 
-window = sg.Window("ZIP compressor", layout=[[label1,input1,choose_button1],[label2,input2,choose_button2],[compress_button]], element_justification="center")
+label3 = sg.Text("", key="output")
+
+window = sg.Window("ZIP compressor", layout=[[label1,input1,choose_button1],[label2,input2,choose_button2],[compress_button,label3]], element_justification="center")
+
+while True:
+    event, values = window.read()
+    print(event,values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+
+    ZIP.make_archive(filepaths,folder)
+    window["output"].update("File compressed sucessfully!")
+
+
+
 window.read()
 window.close()
